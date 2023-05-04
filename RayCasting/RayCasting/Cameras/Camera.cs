a494486062c7ac22d7ad162b577187cdf55b3a05
Melkye <60851956+Melkye@users.noreply.github.com>
2023-05-04 21:02:16 +0300

    add camera

---
+++ RayCasting/RayCasting/Cameras/Camera.cs
@@ -0,0 +1,18 @@
+﻿using RayCasting.Objects;
+
+namespace RayCasting.Cameras;
+internal class Camera : ICamera
+{
+    public Camera(Point3D position, Vector3D direction, float fieldOfView)
+    {
+        Position = position;
+        Direction = direction.Normalized();
+        FieldOfView = fieldOfView;
+    }
+
+    public Point3D Position { get; }
+
+    public Vector3D Direction { get; }
+
+    public float FieldOfView { get; }
+}
---
+++ RayCasting/RayCasting/Cameras/ICamera.cs
@@ -0,0 +1,11 @@
+﻿using RayCasting.Objects;
+
+namespace RayCasting.Cameras;
+internal interface ICamera
+{
+    Point3D Position { get; }
+
+    Vector3D Direction { get; }
+
+    float FieldOfView { get; }
+}
