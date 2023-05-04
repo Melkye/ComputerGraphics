59372cdb8d770786f6fbdd4da42b30d29f68fc60
Melkye <60851956+Melkye@users.noreply.github.com>
2023-05-04 21:03:23 +0300

    add renderer and output to console in main

--- RayCasting/RayCasting/Program.cs
+++ RayCasting/RayCasting/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RayCasting;
+using RayCasting.Objects;
+using RayCasting.Lighting;
+using RayCasting.Figures;
+using RayCasting.Cameras;
+using RayCasting.Scenes;
+using RayCasting.Casters;
+
+
+//var vec1Norm = new Vector3D(1, 0.5f, -1).Normalized();
+//var vec2Norm = new Vector3D(-1, 0.5f, -1).Normalized();
+
+//var angles1 = vec1Norm.GetAngles();
+//var angles2 = vec2Norm.GetAngles();
+
+//var vec1 = new Vector3D(angles1);
+//var vec2 = new Vector3D(angles2);
+
+
+Camera cam1 = new(new(0, 0, -4), new(0, 0, -1), 45);
+Screen screen1 = new(cam1, 50, 50);
+
+Sphere sphere1 = new(new(0, 0, -5), 0.4f);
+//var bla = sphere1.GetIntersectionPoint(new(new(0, 0, 0), new(0.7745966f, -0.44721356f, 0.44721356f)));
+
+Scene scene1 = new(cam1, null, screen1, sphere1);
+
+Renderer renderer = new(scene1, new LightNeglectingCaster());
+
+byte[,] image = renderer.Render();
+
+for (int i = 0; i < image.GetLength(0); i++)
+{
+    for (int j = 0; j < image.GetLength(1); j++)
+    {
+        char symbol = image[i, j] == 0 ? ' ' : '#';
+        Console.Write(symbol);
+    }
+    Console.Write('\n');
+}
---
+++ RayCasting/RayCasting/Renderer.cs
@@ -0,0 +1,63 @@
+﻿using RayCasting.Casters;
+using RayCasting.Objects;
+using RayCasting.Scenes;
+
+namespace RayCasting;
+internal class Renderer
+{
+    public Renderer(Scene scene, ICaster caster)
+    {
+        Scene = scene; // TODO: create copy constructors?
+        Caster = caster;
+    }
+
+    public Scene Scene { get; }
+
+    public ICaster Caster { get; }
+
+    // TODO: move pixel casting corresponding to cam pos and coords
+    public byte[,] Render () // TODO: change output to color?
+    {
+        byte[,] image = new byte[Scene.Screen.Height, Scene.Screen.Width];
+
+        // get vector with starting angle
+        // move by alpha/width or alpha/height
+
+        // TODO: change to hFov and vFov?
+        float pixelWidthAngle = (Scene.Camera.FieldOfView / Scene.Screen.Width) * (float)(Math.PI / 180);
+        float pixelHeightAngle = (Scene.Camera.FieldOfView / Scene.Screen.Height) * (float)(Math.PI / 180);
+
+
+        (float alpha, float beta, float gamma) cameraDirectionAngles = Scene.Camera.Direction.GetAngles();
+
+        // adding half of angle to target the middle of pixel
+        (float alpha, float beta, float gamma) leftTopmostPixelAngles = (
+            cameraDirectionAngles.alpha - pixelWidthAngle * Scene.Screen.Width / 2, // + pixelWidthAngle / 2,
+            cameraDirectionAngles.beta + pixelHeightAngle * Scene.Screen.Height / 2, // + pixelWidthAngle / 2,
+            cameraDirectionAngles.gamma);;
+
+        for (int i = 0; i < image.GetLength(0); i++)
+        {
+            (float alpha, float beta, float gamma) leftSidePixelAngles = (
+            leftTopmostPixelAngles.alpha,
+            leftTopmostPixelAngles.beta - pixelHeightAngle * i,
+            leftTopmostPixelAngles.gamma);
+
+            for (int j = 0; j < image.GetLength(1); j++)
+            {
+                (float alpha, float beta, float gamma) currentPixelAngles = (
+                    leftSidePixelAngles.alpha + pixelWidthAngle * j,
+                    leftSidePixelAngles.beta,
+                    leftSidePixelAngles.gamma);
+                //Point3D pixelPoint = new(); // TODO: find pixel coords
+                //image[i, j] = Caster.Cast(Scene, pixelPoint);
+
+                // TODO: create with pixel angle
+                image[i, j] = Caster.Cast(Scene, currentPixelAngles);
+            }
+        }
+
+        return image;
+    }
+    
+}
