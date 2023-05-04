6a7125ce9c2a95be6948139d4df178741acdc67f
Melkye <60851956+Melkye@users.noreply.github.com>
2023-05-04 21:02:32 +0300

    add scene

---
+++ RayCasting/RayCasting/Scenes/IScreen.cs
@@ -0,0 +1,11 @@
+﻿using RayCasting.Cameras;
+
+namespace RayCasting.Scenes;
+
+internal interface IScreen
+{
+    ICamera Camera { get; }
+    int Height { get; }
+    //Point3D[,] Image { get; }// TODO: need this?
+    int Width { get; }
+}
---
+++ RayCasting/RayCasting/Scenes/Scene.cs
@@ -0,0 +1,22 @@
+﻿using RayCasting.Figures;
+using RayCasting.Lighting;
+using RayCasting.Cameras;
+
+namespace RayCasting.Scenes;
+internal class Scene
+{
+    public Scene(ICamera camera, ILightSource lightSource, IScreen screen, IIntersectable figure)
+    {
+        Camera = camera;
+        LightSource = lightSource;
+        Screen = screen;
+        Figure = figure;
+    }
+    public ICamera Camera { get; }
+
+    public ILightSource LightSource { get; } // TODO: make several sources
+
+    public IIntersectable Figure { get; } // TODO: make several figures
+
+    public IScreen Screen { get; }
+}
