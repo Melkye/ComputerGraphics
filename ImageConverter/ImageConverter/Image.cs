75d72d6224455550c23e09760200974f0029c1e6
Vitalii <shaddyreaper@gmail.com>
2023-05-10 21:16:07 +0300

    change image add hieght, width and colorMaxValue

--- ImageConverter/ImageConverter/Image.cs
+++ ImageConverter/ImageConverter/Image.cs
@@ -2,8 +2,10 @@
 
 public class Image
 {
-    private Pixel[,] Pixelmap { get; }
-
+    public Pixel[,] Pixelmap { get; }
+    public int ColorMaxValue { get => 255; }
+    public int Hieght => Pixelmap.GetLength(0);
+    public int Width => Pixelmap.GetLength(1);
     public Image(Pixel[,] pixelmap)
     {
         Pixelmap = pixelmap;
