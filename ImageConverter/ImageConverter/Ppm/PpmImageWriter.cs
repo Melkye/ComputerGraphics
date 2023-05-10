fff54aecc51c4cf359f2a9a6e3b6f1084075984a
Vitalii <shaddyreaper@gmail.com>
2023-05-10 21:27:00 +0300

    add ppm writer

---
+++ ImageConverter/ImageConverter/Ppm/PpmImageWriter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageConverter.Ppm
+{
+    internal class PpmImageWriter : IImageWriter
+    {
+        private const string fileFormatNumber = "P3";
+        public void Write(Image image, string destination)
+        {
+            StreamWriter streamWriter = new StreamWriter(destination);
+            streamWriter.Write(fileFormatNumber.ToString() + "\n");
+            streamWriter.Write(image.Hieght.ToString() + " ");
+            streamWriter.Write(image.Width.ToString() + "\n");
+            streamWriter.Write(image.ColorMaxValue.ToString() + "\n");
+            for (int i = 0; i < image.Hieght; i++)
+            {
+                for (int j = 0; j < image.Width; j++)
+                {
+                    streamWriter.Write(image[i, j].Red.ToString() + " ");
+                    streamWriter.Write(image[i, j].Green.ToString() + " ");
+                    streamWriter.Write(image[i, j].Blue.ToString() + " ");
+                }
+                streamWriter.Write("\n");
+            }
+            streamWriter.Close();
+        }
+    }
+}
