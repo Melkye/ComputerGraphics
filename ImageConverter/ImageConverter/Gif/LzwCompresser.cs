790fe4f1a711ae52b8a501c36d7c0ff8cf03fb0c
Melkye <60851956+Melkye@users.noreply.github.com>
2023-05-15 15:41:41 +0300

    add lzw compresser stub

---
+++ ImageConverter/ImageConverter/Gif/LzwCompresser.cs
@@ -0,0 +1,12 @@
+namespace ImageConverter.Gif;
+
+public class LzwCompresser
+{
+    public byte[] Compress(byte[] data, int codeSize) => throw new NotImplementedException();
+    public byte[] Decompress(byte[] data, int codeSize)
+    {
+        Dictionary<byte, byte> colorsDic = new();
+
+        return Array.Empty<byte>();
+    }
+}
