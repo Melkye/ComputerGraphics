1c553ff4c45ad8ce298486f0692f2cbe36865125
YarikTalko <ariktalko@gmail.com>
2023-05-18 18:22:21 +0300

    Console Input
    add new class, add launchSettings.json, edit program.cs

---
+++ ImageConverter/ImageConverter/Input.cs
@@ -0,0 +1,105 @@
+﻿using ImageConverter.Bmp;
+using ImageConverter.Ppm;
+
+namespace ImageConverter
+{
+    internal class Input
+    {
+        public Input()
+        {
+
+        }
+
+        public void InputCheck(string[] args)
+        {
+            string source = "";
+            string destination = "";
+            string goal_format = "";
+            bool isDestinationParametеrExist = false;
+            bool isSourceParameterExist = false;
+            bool isGoalFormatParameterExist = false;
+
+            if (args.Length > 6)
+            {
+                throw new Exception("Too many parameters");
+            }
+
+            for (int i = 0; i < args.Length; i++)
+            {
+                if (args[i] == "--destination")
+                {
+                    destination = args[i + 1];
+                    isDestinationParametеrExist = true;
+                }
+                if (args[i] == "--source")
+                {
+                    source = args[i + 1];
+                    isSourceParameterExist = true;
+                }
+                if (args[i] == "--goal-format")
+                {
+                    goal_format = args[i + 1];
+                    isGoalFormatParameterExist = true;
+                }
+            }
+
+            if (!isDestinationParametеrExist)
+            {
+                throw new Exception("you forget --destination");
+            }
+            if (!isSourceParameterExist)
+            {
+                throw new Exception("you forget --source");
+            }
+            if (!isGoalFormatParameterExist)
+            {
+                throw new Exception("you forget --goal-format");
+            }
+            if (!File.Exists(source))
+            {
+                throw new Exception("source is not exists");
+            }
+            if (Path.GetExtension(destination) != ".bmp" && Path.GetExtension(destination) != ".ppm")
+            {
+                throw new Exception("destination is wrong");
+            }
+            if (goal_format != ".bmp" && goal_format != ".ppm")
+            {
+                throw new Exception("goal-format must be .ppm or .bmp");
+            }
+            if (Path.GetExtension(destination) == Path.GetExtension(source))
+            {
+                throw new Exception("destination file extension cannot be the same as source file extension");
+            }
+            if (Path.GetExtension(destination) != goal_format)
+            {
+                throw new Exception("destination file extension must be the same as goal-format");
+            }
+            if (string.IsNullOrEmpty(destination) || string.IsNullOrEmpty(source) || string.IsNullOrEmpty(goal_format))
+            {
+                throw new Exception("destination, source and goal-format must not be null or empty");
+            }
+
+            Console.WriteLine(destination + " " + source + " " + goal_format);
+
+            Choise(destination, source, goal_format);
+
+        }
+
+        private void Choise(string destination, string source, string goal_format)
+        {
+            ImageConverter ic;
+
+            if (goal_format == ".bmp")
+            {
+                ic = new(new PpmImageReader(), new BmpImageWriter());
+            }
+            else
+            {
+                ic = new(new BmpImageReader(), new PpmImageWriter());
+            }
+
+            ic.Convert(source, goal_format, destination);
+        }
+    }
+}
--- ImageConverter/ImageConverter/Program.cs
+++ ImageConverter/ImageConverter/Program.cs
@@ -1,54 +1,12 @@
 ﻿using ImageConverter;
-using ImageConverter.Bmp;
-using ImageConverter.Ppm;
 
 internal class Program
 {
-    private static void Main(string[] args) // --source smth --goal-format smth --output smth
+    private static void Main(string[] args)
     {
-        string source = "";
-        string destination = "";
-        string goal_format = "";
+        Input input = new Input();
+        input.InputCheck(args);
 
-        string basePath = System.IO.Directory.GetCurrentDirectory();
-        //string readLine = Console.ReadLine();
-        //string[] parameters = readLine.Split(' ', '=');
-        //for (int i = 0; i < args.Length; i++)
-        //{
-        //    Console.WriteLine(args[i]);
-        //}
-
-        // TODO: check is user doesn't specify argument
-        for (int i = 0; i < args.Length - 1; i++)
-        {
-            if (args[i] == "--output")
-            {
-                destination = args[i + 1];
-                i++;
-            }
-            if (args[i] == "--source")
-            {
-                source = args[i + 1];
-                i++;
-            }
-            if (args[i] == "--goal-format")
-            {
-                goal_format = args[i + 1];
-                i++;
-            }
-        }
-
-        //C:\Users\arikt\Documents\Programming\ComputerGraphics\ImageConverter\Images\MARBLES.ppm
-        //C:\Users\arikt\Documents\Programming\ComputerGraphics\ImageConverter\Images\output1.bmp
-
-        //source = "C:\\Users\\arikt\\Documents\\Programming\\ComputerGraphics\\ImageConverter\\Images\\MARBLES.ppm";
-        //destination = "C:\\Users\\arikt\\Documents\\Programming\\ComputerGraphics\\ImageConverter\\Images\\output1.bmp";
-
-        // TODO: create converter based on passed args
-        // NOTE: consider using converter factory
-
-        ImageConverter.ImageConverter ic = new(new PpmImageReader(), new BmpImageWriter());
-
-        ic.Convert(source, goal_format, destination);
+        string bastePath = System.IO.Directory.GetCurrentDirectory();
     }
 }
