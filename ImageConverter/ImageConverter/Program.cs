78077c9dfdf0435fcb08df1d4658f8cefc2b4307
Melkye <60851956+Melkye@users.noreply.github.com>
2023-05-24 11:06:43 +0300

    fix command lone args parsing

--- ImageConverter/ImageConverter/Program.cs
+++ ImageConverter/ImageConverter/Program.cs
@@ -1,11 +1,33 @@
-﻿using ImageConverter.Bmp;
-using ImageConverter.Gif;
 using ImageConverter;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
-        Input input = new Input(args);
+        (string source, string goalFormat, string destination) = ("", "", "");
+        try
+        {
+            (source, goalFormat, destination) = new CommandLineArgsParser().ParseArgs(args);
+        }
+        catch (ArgumentException e)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(e.Message);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        try
+        {
+            ImageConverter.ImageConverter ic = new();
+
+            ic.Convert(source, goalFormat, destination);
+        }
+        catch (Exception e)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(e.Message);
+            Console.ForegroundColor = ConsoleColor.White;
+
+        }
     }
 }
