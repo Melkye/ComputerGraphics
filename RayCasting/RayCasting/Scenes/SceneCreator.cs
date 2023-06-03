0e1ba5edc590d22282fe446d2b91173f074c2a7b
Melkye <60851956+Melkye@users.noreply.github.com>
2023-06-03 10:54:02 +0300

    factor out triangles heaven to scene creator

--- RayCasting/RayCasting/Scenes/SceneCreator.cs
+++ RayCasting/RayCasting/Scenes/SceneCreator.cs
@@ -6,7 +6,7 @@ namespace RayCasting.Scenes;
 
 public class SceneCreator
 {
-    public Scene Create9Shperes()
+    public Scene Create9Spheres()
     {
         Camera camera = new(new(0, 0, 0), new(0, 0, -1), new(1, 0, 0), 60);
         ILightSource lightSource = new DirectedLightSource(new(0, 1, 0), new(0, -1, 0));
@@ -26,5 +26,49 @@ public class SceneCreator
         Scene scene = new(camera, lightSource, figures);
 
         return scene;
+    }
+
+    public Scene TriangleHell()
+    {
+        Camera camera = new(new(0, 0, 0), new(0, 0, -1), new(1, 0, 0), 90);
+        ILightSource downsideLight = new DirectedLightSource(new(0, 1, 0), new(0, -1, 0));
+
+        float coordZ = -10;
+
+        Scene triangleHell = new(camera, downsideLight,
+        new IIntersectable[]
+        {
+            new Triangle(new(-5f, 4f, coordZ), new(-4f, 6f, coordZ), new(-3f,4f, coordZ)),
+            new Triangle(new(-3f, 4f, coordZ), new(-2f, 6f, coordZ), new(-1f, 4f, coordZ)),
+            new Triangle(new(-1f, 4f, coordZ), new(0f, 6f, coordZ), new(1f, 4f, coordZ)),
+            new Triangle(new(1f, 4f, coordZ), new(2f, 6f, coordZ), new(3f, 4f, coordZ)),
+            new Triangle(new(3f, 4f, coordZ), new(4f, 6f, coordZ), new(5f, 4f, coordZ)),
+
+            new Triangle(new(-5f, 2f, coordZ), new(-4f, 4f, coordZ), new(-3f, 2f, coordZ)),
+            new Triangle(new(-3f, 2f, coordZ), new(-2f, 4f, coordZ), new(-1f, 2f, coordZ)),
+            new Triangle(new(-1f, 2f, coordZ), new(0f, 4f, coordZ), new(1f, 2f, coordZ)),
+            new Triangle(new(1f, 2f, coordZ), new(2f, 4f, coordZ), new(3f, 2f, coordZ)),
+            new Triangle(new(3f, 2f, coordZ), new(4f, 4f, coordZ), new(5f, 2f, coordZ)),
+
+            new Triangle(new(-5f, 0f, coordZ), new(-4f, 2f, coordZ), new(-3f, 0f, coordZ)),
+            new Triangle(new(-3f, 0f, coordZ), new(-2f, 2f, coordZ), new(-1f, 0f, coordZ)),
+            new Triangle(new(-1f, 0f, coordZ), new(0f, 2f, coordZ), new(1f, 0f, coordZ)),
+            new Triangle(new(1f, 0f, coordZ), new(2f, 2f, coordZ), new(3f, 0f, coordZ)),
+            new Triangle(new(3f, 0f, coordZ), new(4f, 2f, coordZ), new(5f, 0f, coordZ)),
+
+            new Triangle(new(-5f, -2f, coordZ), new(-4f, 0f, coordZ), new(-3f, -2f, coordZ)),
+            new Triangle(new(-3f, -2f, coordZ), new(-2f, 0f, coordZ), new(-1f, -2f, coordZ)),
+            new Triangle(new(-1f, -2f, coordZ), new(0f, 0f, coordZ), new(1f, -2f, coordZ)),
+            new Triangle(new(1f, -2f, coordZ), new(2f, 0f, coordZ), new(3f, -2f, coordZ)),
+            new Triangle(new(3f, -2f, coordZ), new(4f, 0f, coordZ), new(5f, -2f, coordZ)),
+
+            new Triangle(new(-5f, -4f, coordZ), new(-4f, -2f, coordZ), new(-3f, -4f, coordZ)),
+            new Triangle(new(-3f, -4f, coordZ), new(-2f, -2f, coordZ), new(-1f, -4f, coordZ)),
+            new Triangle(new(-1f, -4f, coordZ), new(0f, -2f, coordZ), new(1f, -4f, coordZ)),
+            new Triangle(new(1f, -4f, coordZ), new(2f, -2f, coordZ), new(3f, -4f, coordZ)),
+            new Triangle(new(3f, -4f, coordZ), new(4f, -2f, coordZ), new(5f, -4f, coordZ)),
+        });
+
+        return triangleHell;
     }
 }
